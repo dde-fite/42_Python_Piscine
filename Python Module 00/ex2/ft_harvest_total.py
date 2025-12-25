8697e27dcc68712cfa5d8bdac42b11f44c90bcd6
dde-fite <nora@defitero.com>
2025-12-25 04:59:08 +0100

    use of template string in final total harvest ex2

--- Python Module 00/ex2/ft_harvest_total.py
+++ Python Module 00/ex2/ft_harvest_total.py
@@ -1,4 +1,4 @@
 def ft_harvest_total(harvest: int = 0):
     for i in (1, 2, 3):
         harvest += int(input(f"Day {i} harvest: "))
-    print("Total harvest:", harvest)
+    print(f"Total harvest: {harvest}")
