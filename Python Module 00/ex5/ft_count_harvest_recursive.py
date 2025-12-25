827eff9c7f169bf476ece62a1e9ad5742efff6de
dde-fite <nora@defitero.com>
2025-12-25 05:05:45 +0100

    use of fstrings in whole project

--- Python Module 00/ex5/ft_count_harvest_recursive.py
+++ Python Module 00/ex5/ft_count_harvest_recursive.py
@@ -2,7 +2,7 @@ def ft_count_harvest_recursive():
     def harvst_recursion(day: int):
         if day > 1:
             harvst_recursion(day - 1)
-        print("Day", day)
+        print(f"Day {day}")
     days = int(input("Days until harvest: "))
     if days > 0:
         harvst_recursion(days)
