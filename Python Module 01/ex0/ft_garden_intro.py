cd4b746d4a031fa672b5184cff52e63024967f02
dde-fite <nora@defitero.com>
2025-12-26 01:06:41 +0100

    ex0 done

---
+++ Python Module 01/ex0/ft_garden_intro.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+if __name__ == "__main__":
+    name: str = "Rose"
+    height: str = "25cm"
+    age: str = "30 days"
+    print("=== Welcome to My Garden ===\n"
+          f"Plant: {name}\n"
+          f"Height: {height}\n"
+          f"Age: {age}\n"
+          "\n=== End of Program ===")
