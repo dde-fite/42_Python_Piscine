9ec0e643ec754e05d2e990e16e5edd3446cb899f
dde-fite <nora@defitero.com>
2025-12-26 02:10:21 +0100

    corrected 01-ex01

--- Python Module 01/ex1/ft_garden_data.py
+++ Python Module 01/ex1/ft_garden_data.py
@@ -3,9 +3,12 @@ class Plant:
         self.name: str = name
         self.height: int = height
         self.age: int = age
-        print(f"{self.name}: {self.height}cm, {self.age} days old")
 
 
-# plant1 = Plant("Rose", 25, 30)
-# plant2 = Plant("Sunflower", 80, 45)
-# plant3 = Plant("Cactus", 15, 120)
+plant1 = Plant("Rose", 25, 30)
+plant2 = Plant("Sunflower", 80, 45)
+plant3 = Plant("Cactus", 15, 120)
+print("=== Garden Plant Registry ===\n"
+      f"{plant1.name}: {plant1.height}cm, {plant1.age} days old\n"
+      f"{plant2.name}: {plant2.height}cm, {plant2.age} days old\n"
+      f"{plant3.name}: {plant3.height}cm, {plant3.age} days old")
