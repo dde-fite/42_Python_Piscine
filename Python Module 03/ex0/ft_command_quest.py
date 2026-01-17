bb1913e4088c37383ee46d5ebd930db95bcb7e49
Nora de Fitero Teijeira <nora@defitero.com>
2026-01-17 20:35:15 +0100

    copied argv for unmutability

--- Python Module 03/ex0/ft_command_quest.py
+++ Python Module 03/ex0/ft_command_quest.py
@@ -1,11 +1,11 @@
 import sys
 
-argv: list[str] = sys.argv
+argv: list[str] = sys.argv.copy()
 argc: int = len(argv)
 
 print("=== Command Quest ===")
 print(f"Program name: {argv.pop(0)}")
-if argc > 2:
+if argc > 1:
     print(f"Arguments received: {argc - 1}")
     for arg in argv:
         print(f"Argument {argv.index(arg)}: {arg}")
