02e444ae326497229b1aad7da07375ec6cb57b94
David de Fitero Teijeira <dde-fite@student.42madrid.com>
2026-01-19 13:49:52 +0100

    03-ex3 done

--- Python Module 03/ex3/ft_achievement_tracker.py
+++ Python Module 03/ex3/ft_achievement_tracker.py
@@ -1,10 +1,20 @@
 def print_player_achievements(players: dict[str, set[str]], player: str
                               ) -> None:
+    """Prints all the achivements of a player in a dictionary.
+
+    Args:
+        players (dict[str, set[str]]): Achievements of each player.
+        player (str): Name of the player.
+    """
     print(f"Player {player} achievements: {players[player]}")
 
 
 def print_stats(players: dict[str, set[str]]) -> None:
+    """Prints all the stats of the achievements.
 
+    Args:
+        players (dict[str, set[str]]): Achievements of each player.
+    """
     all_sets = list(players.values())
     unique: set[str] = set().union(*all_sets)
     common: set[str] = set.intersection(*all_sets)
@@ -25,9 +35,22 @@ def print_stats(players: dict[str, set[str]]) -> None:
           f"Rare achievements (1 player): {rare}")
 
 
-def compare_players(players: dict[str, set[str]], player_a: set[str],
-                    player_b: set[str]) -> None:
-    print()
+def compare_players(players: dict[str, set[str]], player_a: str,
+                    player_b: str) -> None:
+    """Compares the achievements of two players given.
+
+    Args:
+        players (dict[str, set[str]]): Achievements of each player.
+        player_a (str): Name of player A.
+        player_b (str): Name of player B.
+    """
+    common: set[str] = players[player_a].intersection(players[player_b])
+    a_unique: set[str] = players[player_a].difference(players[player_b])
+    b_unique: set[str] = players[player_b].difference(players[player_a])
+    print(f"\n{player_a.capitalize()} vs {player_b.capitalize()} "
+          f"common: {common}")
+    print(f"{player_a.capitalize()} unique: {a_unique}")
+    print(f"{player_b.capitalize()} unique: {b_unique}")
 
 
 players = {
@@ -42,3 +65,4 @@ print_player_achievements(players, "alice")
 print_player_achievements(players, "bob")
 print_player_achievements(players, "charlie")
 print_stats(players)
+compare_players(players, "alice", "bob")
