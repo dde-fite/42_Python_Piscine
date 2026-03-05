9fbe2f2517c44c3ccec3203b42290374e74d136f
dde-fite <nora@defitero.com>
2026-03-05 23:13:53 +0100

    07-ex1 pending

---
+++ Python Module 07/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa: F401
+
+from .ex0 import Card, CreatureCard
+from .ex1 import SpellCard, ArtifactCard, Deck
---
+++ Python Module 07/ex1/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa: F401
+
+from .SpellCard import SpellCard
+from .ArtifactCard import ArtifactCard
+from .Deck import Deck
